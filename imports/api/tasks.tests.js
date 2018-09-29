db231fec42083ee865a689dd90fb1f9cbe48de06
enjogu <enjogu333@gmail.com>
2018-09-29 21:17:01 +0100

    Prepare the database for each test

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -1,9 +1,13 @@
 /* eslint-env mocha */
 
+ import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+
  
 
-import { Meteor } from 'meteor/meteor';
+import { Tasks } from './tasks.js';
 
+ 
  
 
 if (Meteor.isServer) {
@@ -11,7 +15,31 @@ if (Meteor.isServer) {
   describe('Tasks', () => {
 
     describe('methods', () => {
+      const userId = Random.id();
 
+      let taskId;
+
+ 
+
+      beforeEach(() => {
+
+        Tasks.remove({});
+
+        taskId = Tasks.insert({
+
+          text: 'test task',
+
+          createdAt: new Date(),
+
+          owner: userId,
+
+          username: 'tmeasday',
+
+        });
+
+      });
+
+ 
       it('can delete owned task', () => {
 
       });
